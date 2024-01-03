4b66dc57779ed2fb88b0fa3193ae43a710024477
SipYoHONG <qkqkqk1235@naver.com>
2024-01-03 10:44:59 +0900

    111

---
+++ 03.JavaScript/ch01_basic/03_data_type.js
@@ -0,0 +1,19 @@
+// 자바스크립트의 자료형
+
+// 1. Number
+
+// 마우스로 다 잡고 alt shift a면 다 주석처리
+/* console.log(5 +2);
+console.log(5 + 2, 5 -2, 5 * 2, 5 / 2); // +, -, *, /
+console.log(5 % 2); //modulo: 나머지
+console.log(2 ** 10); // 2의 10제곱, 1024(1k)
+console.log(1.3e-8); // 1.3 x 10 ** -8 */
+
+// 2. 문자열
+console.log('He said "I love you"');
+console.log("i'll be back");
+console.log("He said \"I love you\"") // escaping
+console.log("Back slash(\\) 기호를 사용할 때에는 \\ 두개를 쓰면 됩니다.");
+
+let hello = "안녕하세요?";
+console.log(hello[2], hello[5]) // 시작은 0부터 
