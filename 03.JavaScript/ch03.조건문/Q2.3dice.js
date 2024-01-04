9185b91cd0419d9a1c96064723f2678786547ee2
SipYoHONG <qkqkqk1235@naver.com>
2024-01-04 09:32:44 +0900

    1111

--- 03.JavaScript/ch03.조건문/Q2.3dice.js
+++ 03.JavaScript/ch03.조건문/Q2.3dice.js
@@ -7,13 +7,11 @@ let prize = 0;
 
 if (dice1 == dice2 && dice2 == dice3) {
     prize = 10000 * dice1 * 1000;
-} else if (dice1 == dice2) {
+} else if (dice1 == dice2 || dice1 == dice3) {
     prize = 1000 + dice1 * 100;
 } else if (dice2 == dice3) {
     prize = 1000 + dice2 * 100;
-} else if (dice1 == dice3) {
-    prize = 1000 + dice1 * 100;
-} else {
+}  else {
     let maxDice = dice1 > dice2 ? dice1 : dice2;
     maxDice = maxDice > dice3 ? maxDice : dice3;
     prize = maxDice * 100;
