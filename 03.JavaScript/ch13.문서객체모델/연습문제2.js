a362260f654dc56b91753f1e5797c97b6f4cb261
SipYoHONG <qkqkqk1235@naver.com>
2024-01-09 20:52:33 +0900

    1111

--- 03.JavaScript/ch13.문서객체모델/연습문제2.js
+++ 03.JavaScript/ch13.문서객체모델/연습문제2.js
@@ -1,4 +1,4 @@
-const inputString = "The quick brown fox jumped over the lazy dog. The dog barked, and then the fox ran away.";
+const inputString = "the grown-ups' response, this time, was to advise me to lay aside my drawings of boa constrictors, whether from the inside or the outside, and devote myself instead to geography, history, arithmetic, and grammar. That is why, at the, age of six, I gave up what might have been a magnificent career as a painter. I had been disheartened by the failure of my Drawing Number One and my Drawing Number Two. Grown-ups never understand anything by themselves, and it is tiresome for children to be always and forever explaining things to the.";
 
 
 function countTheOccurrences(inputString) {
